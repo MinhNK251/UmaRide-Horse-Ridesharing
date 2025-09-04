69a287eaf1f597c9fa95fc933119b28c995db3e2
Nguyễn Khánh Minh <nguyenkhanhminh251@gmail.com>
2025-09-04 20:14:07 +0700

    Fix Nativewind import

--- uma-ride/app/(tabs)/test.tsx
+++ uma-ride/app/(tabs)/test.tsx
@@ -3,8 +3,8 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-white dark:bg-black">
-      <Text className="text-xl font-bold text-blue-500 dark:text-blue-300">
+    <SafeAreaView className="flex-1 items-center justify-center bg-white dark:bg-white">
+      <Text className="text-xl font-bold text-blue-500 dark:text-red-500">
         Welcome to Nativewind, Please use light mode to see the test!
       </Text>
     </SafeAreaView>
