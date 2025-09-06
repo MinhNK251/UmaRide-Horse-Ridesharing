903e565e4c2784920a20486de4ad7fb376d3551c
Nguyễn Khánh Minh <nguyenkhanhminh251@gmail.com>
2025-09-06 14:12:38 +0700

    Update Onboarding Screen
    Install react-native-swiper, Add constants, Add CustomButton, Update Onboarding Screen UI

--- uma-ride/app/(auth)/_layout.tsx
+++ uma-ride/app/(auth)/_layout.tsx
@@ -5,7 +5,7 @@ import "react-native-reanimated";
 const Layout = () => {
   return (
     <Stack>
-      <Stack.Screen name="welcome" options={{ headerShown: false }} />
+      <Stack.Screen name="onboarding" options={{ headerShown: false }} />
       <Stack.Screen name="sign-up" options={{ headerShown: false }} />
       <Stack.Screen name="sign-in" options={{ headerShown: false }} />
     </Stack>
