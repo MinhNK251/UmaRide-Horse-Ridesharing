1cf5cd54b5b4e433a3ffe0583549e23531441fc0
Nguyễn Khánh Minh <nguyenkhanhminh251@gmail.com>
2025-09-06 14:54:26 +0700

    Update Sign In Screen

--- uma-ride/app/(auth)/sign-in.tsx
+++ uma-ride/app/(auth)/sign-in.tsx
@@ -1,19 +1,19 @@
-import { Link, router } from "expo-router";
-import { Image, ScrollView, Text, View } from "react-native";
 import CustomButton from "@/components/CustomButton";
-import { icons, images } from "@/constants";
 import InputField from "@/components/InputField";
-import { useState } from "react";
 import OAuth from "@/components/OAuth";
+import { icons, images } from "@/constants";
+import { Link } from "expo-router";
+import { useState } from "react";
+import { ScrollView, Text, View, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-const SignUp = () => {
+const SignIn = () => {
   const [form, setForm] = useState({
-    name: "",
     email: "",
     password: "",
   });
 
-  const onSignUpPress = async () => {};
+  const onSignInPress = async () => {};
 
   return (
     <ScrollView className="flex-1 bg-white">
@@ -21,17 +21,10 @@ const SignUp = () => {
         <View className="relative w-full h-[220px]">
           <Image source={images.signUpHorse} className="z-0 w-full h-[220px]" />
           <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5 bg-green-100 px-3 py-3 rounded-2xl">
-            Create Your Account
+            Welcome Back 👋
           </Text>
         </View>
-        <View className="px-5">
-          <InputField
-            label="Name"
-            placeholder="Enter your name"
-            icon={icons.person}
-            value={form.name}
-            onChangeText={(value) => setForm({ ...form, name: value })}
-          />
+        <View className="p-5">
           <InputField
             label="Email"
             placeholder="Enter your email"
@@ -51,19 +44,19 @@ const SignUp = () => {
           />
 
           <CustomButton
-            title="Sign Up"
-            className="mt-4"
-            onPress={onSignUpPress}
+            title="Sign In"
+            className="mt-6"
+            onPress={onSignInPress}
           />
 
           <OAuth />
 
           <Link
-            href="/sign-in"
-            className="text-lg text-center text-general-200 mt-5"
+            href="/sign-up"
+            className="text-lg text-center text-general-200 mt-6"
           >
-            Already have an account?{" "}
-            <Text className="text-primary-500">Log in</Text>
+            Don&apos;t have an account?{" "}
+            <Text className="text-primary-500">Sign Up</Text>
           </Link>
         </View>
       </View>
@@ -71,4 +64,4 @@ const SignUp = () => {
   );
 };
 
-export default SignUp;
+export default SignIn;
