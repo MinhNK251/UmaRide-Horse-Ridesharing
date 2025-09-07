56f81326b7d4b613d665793a82f7f44d0c629018
Nguyễn Khánh Minh <nguyenkhanhminh251@gmail.com>
2025-09-07 15:30:58 +0700

    Add OTP Verification modals

--- uma-ride/app/(auth)/sign-up.tsx
+++ uma-ride/app/(auth)/sign-up.tsx
@@ -6,9 +6,11 @@ import InputField from "@/components/InputField";
 import { useState } from "react";
 import OAuth from "@/components/OAuth";
 import { useSignUp } from "@clerk/clerk-expo";
+import { ReactNativeModal } from "react-native-modal";
 
 const SignUp = () => {
   const { isLoaded, signUp, setActive } = useSignUp();
+  const [showSuccessModal, setShowSuccessModal] = useState(false);
   const [verification, setVerification] = useState({
     state: "default",
     error: "",
@@ -51,7 +53,7 @@ const SignUp = () => {
       });
       // If verification was completed, set the session to active and redirect the user
       if (signUpAttempt.status === "complete") {
-        // Create a database User
+        // TODO: Create a database User
 
         await setActive({ session: signUpAttempt.createdSessionId });
         setVerification({
@@ -129,6 +131,64 @@ const SignUp = () => {
             <Text className="text-primary-500">Log in</Text>
           </Link>
         </View>
+
+        <ReactNativeModal
+          isVisible={verification.state === "pending"}
+          onModalHide={() => {
+            if (verification.state === "success") {
+              setShowSuccessModal(true);
+            }
+          }}
+        >
+          <View className="bg-white px-7 py-9 rounded-2xl min-h-[300px]">
+            <Text className="font-JakartaExtraBold text-2xl mb-2">
+              Email Verification
+            </Text>
+            <Text className="font-Jakarta">
+              We&apos;ve sent a verification code to {form.email}.
+            </Text>
+            <InputField
+              label={"Code"}
+              icon={icons.lock}
+              placeholder={"12345"}
+              value={verification.code}
+              keyboardType="numeric"
+              onChangeText={(code) =>
+                setVerification({ ...verification, code })
+              }
+            />
+            {verification.error && (
+              <Text className="text-red-500 text-sm mt-1">
+                {verification.error}
+              </Text>
+            )}
+            <CustomButton
+              title="Verify Email"
+              onPress={onPressVerify}
+              className="mt-5 bg-success-500"
+            />
+          </View>
+        </ReactNativeModal>
+
+        <ReactNativeModal isVisible={showSuccessModal}>
+          <View className="bg-white px-7 py-9 rounded-2xl min-h-[300px]">
+            <Image
+              source={images.check}
+              className="w-[110px] h-[110px] mx-auto my-5"
+            />
+            <Text className="text-3xl font-JakartaBold text-center">
+              Verified
+            </Text>
+            <Text className="text-base text-gray-400 font-Jakarta text-center mt-2">
+              You have successfully verified your account.
+            </Text>
+            <CustomButton
+              title="Browse Home"
+              onPress={() => router.push(`/(root)/(tabs)/home`)}
+              className="mt-5"
+            />
+          </View>
+        </ReactNativeModal>
       </View>
     </ScrollView>
   );
