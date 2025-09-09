337b231df56ec06a3d4a4b503dafbc2f57dc4551
Nguyễn Khánh Minh <nguyenkhanhminh251@gmail.com>
2025-09-09 18:31:57 +0700

    Add Mock Data

---
+++ uma-ride/constants/mockJockeys.ts
@@ -0,0 +1,45 @@
+export const mockJockeys = [
+  {
+    id: "1",
+    first_name: "Yuga",
+    last_name: "Kawada",
+    profile_image_url:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c2/Yuga_Kawada_2023.jpg/500px-Yuga_Kawada_2023.jpg",
+    horse_image_url:
+      "https://upload.wikimedia.org/wikipedia/commons/d/d9/Lovely_Day_Takarazuka_kinen_2015.jpg",
+    horse_seats: 1,
+    rating: "4.80",
+  },
+  {
+    id: "2",
+    first_name: "Keita",
+    last_name: "Tosaki",
+    profile_image_url:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Keita_Tosaki_2025_Tokyo_Racecourse.jpg/500px-Keita_Tosaki_2025_Tokyo_Racecourse.jpg",
+    horse_image_url:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Akai_Torino_Musume_Yushun_Himba_2021.jpg/500px-Akai_Torino_Musume_Yushun_Himba_2021.jpg",
+    horse_seats: 2,
+    rating: "4.60",
+  },
+  {
+    id: "3",
+    first_name: "Ryusei",
+    last_name: "Sakai",
+    profile_image_url:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Ryusei_Sakai_2023.jpg/500px-Ryusei_Sakai_2023.jpg",
+    horse_image_url:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Stunning_Rose_Shukasho_2022%28IMG2%29.jpg/1024px-Stunning_Rose_Shukasho_2022%28IMG2%29.jpg",
+    horse_seats: 1,
+    rating: "4.70",
+  },
+  {
+    id: "4",
+    first_name: "Ryuji",
+    last_name: "Wada",
+    profile_image_url:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9d/Ryuji_Wada_2022.jpg/500px-Ryuji_Wada_2022.jpg",
+    horse_image_url: "https://www.jbis.jp/horse/images/0000990619_1/",
+    horse_seats: 3,
+    rating: "4.90",
+  },
+];
